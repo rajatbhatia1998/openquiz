09daa9f645dc20afbc52ce17376753cab5bbf468
rajatbhatia1998 <rajat.bhatia859@gmail.com>
2020-07-03 09:48:42 +0530

    Updates

---
+++ precache-manifest.e5dbf5fc16cea3d3f801de6aac9d8130.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "09dbce00f7c5371a4d2b22b3dd740118",
+    "url": "/openquiz/index.html"
+  },
+  {
+    "revision": "9d04b6fa08783cbdd9df",
+    "url": "/openquiz/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "11158590c8e731c4ef2a",
+    "url": "/openquiz/static/js/2.f9a038ab.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/openquiz/static/js/2.f9a038ab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9d04b6fa08783cbdd9df",
+    "url": "/openquiz/static/js/main.fd334679.chunk.js"
+  },
+  {
+    "revision": "5f354052030e72416ec0",
+    "url": "/openquiz/static/js/runtime-main.b283e15f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/openquiz/static/media/logo.5d5d9eef.svg"
+  }
+]);
