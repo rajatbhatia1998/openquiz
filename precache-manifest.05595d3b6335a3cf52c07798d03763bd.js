4b5c613300c1800ed353103f90b28612a6d17dd4
rajatbhatia1998 <rajat.bhatia859@gmail.com>
2020-07-03 19:27:44 +0530

    Updates

---
+++ precache-manifest.05595d3b6335a3cf52c07798d03763bd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e1ccebba7ffabdba76c54f1703e8da5f",
+    "url": "/openquiz/index.html"
+  },
+  {
+    "revision": "b2c1db806e11a8acef98",
+    "url": "/openquiz/static/css/2.c27a359e.chunk.css"
+  },
+  {
+    "revision": "b4492c1d3ae9d9398bab",
+    "url": "/openquiz/static/css/main.8b8b0893.chunk.css"
+  },
+  {
+    "revision": "b2c1db806e11a8acef98",
+    "url": "/openquiz/static/js/2.ca0a7781.chunk.js"
+  },
+  {
+    "revision": "77c675956f6eb2d4b39081194f267529",
+    "url": "/openquiz/static/js/2.ca0a7781.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b4492c1d3ae9d9398bab",
+    "url": "/openquiz/static/js/main.9346f206.chunk.js"
+  },
+  {
+    "revision": "5f354052030e72416ec0",
+    "url": "/openquiz/static/js/runtime-main.b283e15f.js"
+  }
+]);
